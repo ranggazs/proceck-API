0ed1ba3b6d1fb28bb02912c689c442bdbc516d96
ranggazs <74724682+ranggazs@users.noreply.github.com>
2022-07-07 17:27:24 +0800

    Update endpoint.js

--- src/routes/endpoint.js
+++ src/routes/endpoint.js
@@ -6,6 +6,6 @@ export const endpoint = app.get('/', (req,res) => {
     res.status(200).json({
         status: 200,
         message: 'Welcome to Profile Game Checker API 👋',
-        documentation: 'https://github.com/ArieAkbarull/proceck-API'
+        documentation: 'coming soon'
     })
 })
