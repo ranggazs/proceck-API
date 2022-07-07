c5e2e93a5d5d5996dfd41bc9a247653fc4663ed6
ranggazs <74724682+ranggazs@users.noreply.github.com>
2022-07-08 00:55:23 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import checkaccount from './src/routes/checkaccount.js'
-import { endpoint } from 'index.html'
+import { endpoint } from './src/routes/endpoint.js'
 
 const app = express()
 
@@ -8,5 +8,5 @@ app.use(endpoint)
 app.use('/api/', checkaccount)
 
 app.listen(3000, () => {
-    console.log('App Running on http://localhost:3000')
+    console.log('App Running on https://api-alfagames.vercel.app/index.html')
 })
