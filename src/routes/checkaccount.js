467f15ea1919a940182ffe6984163100d026a6a5
ranggazs <74724682+ranggazs@users.noreply.github.com>
2022-07-08 00:33:54 +0800

    Update checkaccount.js

--- src/routes/checkaccount.js
+++ src/routes/checkaccount.js
@@ -3,9 +3,9 @@ import { checkAccountAOV, checkAccountCOD, checkAccountFF, checkAccountGI, check
 
 const checkaccount = express()
 
-checkaccount.get('/ff/:id', checkAccountFF)
-checkaccount.get('/ml/:zoneid/:id', checkAccountML)
-checkaccount.get('/aov/:id', checkAccountAOV)
+checkaccount.get('/freefire/:id', checkAccountFF)
+checkaccount.get('/mobilelegends/:id/:zoneid', checkAccountML)
+checkaccount.get('/arenaofvalor/:id', checkAccountAOV)
 checkaccount.get('/cod/:id', checkAccountCOD)
 checkaccount.get('/gi/:id', checkAccountGI)
 checkaccount.get('/sm/:id', checkAccountSM)
